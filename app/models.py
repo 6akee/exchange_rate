f96f2d135bdb77b8cc581cdc3771ae5439941bc1
6akee <almassov.bauyrzhan@gmail.com>
2023-08-17 17:25:28 +0600

    feat: add userhistory in model

--- app/models.py
+++ app/models.py
@@ -15,9 +15,9 @@ alembic upgrade head
 """
 import uuid
 
-from sqlalchemy import String
+from sqlalchemy import Column, DateTime, ForeignKey, String, func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -34,3 +34,14 @@ class User(Base):
         String(254), nullable=False, unique=True, index=True
     )
     hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
+
+
+class UserHistory(Base):
+    __tablename__ = "user_history"
+
+    id = Column(String, primary_key=True, default=lambda _: str(uuid.uuid4()))
+    user_id = Column(String, ForeignKey("user_model.id"))
+    timestamp = Column(DateTime, server_default=func.now())
+    action = Column(String)
+
+    user = relationship("User", back_populates="history")
